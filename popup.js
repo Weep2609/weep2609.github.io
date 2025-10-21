276d05c4d2d825c8f485fbead0d6260505dc07e3
vv33pzGOq <102238853+Weep2609@users.noreply.github.com>
2025-10-21 11:54:45 +0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
-const WEBHOOK = process.env.DISCORD_WEBHOOK;
-console.log(WEBHOOK);
+const test = process.env.DISCORD_WEBHOOK;
+console.log(test);
 alert(window.origin);
 alert(document.cookie);
 alert('Bug bounty - POC by weep2609');
