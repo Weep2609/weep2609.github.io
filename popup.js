c96d0aa6391ce17c777cf3073d581b97e3318535
vv33pzGOq <102238853+Weep2609@users.noreply.github.com>
2025-10-21 11:46:48 +0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
-const databaseUrl = process.env.DATABASE_URL
-console.log(databaseUrl)
+const databaseUrl = process.env.DATABASE_URL;
+console.log(databaseUrl);
 alert(window.origin);
 alert(document.cookie);
 alert(Bug bounty - POC by weep2609);
